b95112e11e98358f989ff9ce39f1b313eb113f95
Spencer Eldred <eldredspencer@gmail.com>
2014-08-04 10:42:59 -0600

    Added user_id to todo item, fixed story bug

--- to_do_app.rb
+++ to_do_app.rb
@@ -19,7 +19,7 @@ class ToDoApp < Sinatra::Application
       user = current_user
 
       users = User.where("id != #{user.id}")
-      todos = ToDoItem.where(completed: nil)
+      todos = ToDoItem.where(user_id: current_user.id, completed: nil)
       erb :signed_in, locals: {current_user: user, users: users, todos: todos}
     else
       erb :signed_out
@@ -76,7 +76,7 @@ class ToDoApp < Sinatra::Application
   end
 
   post "/todos" do
-    ToDoItem.create(body: params[:body])
+    ToDoItem.create(body: params[:body], user_id: current_user.id)
 
     flash[:notice] = "ToDo added"
 
